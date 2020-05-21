29243bdba747820c1f78264eedacf4ce2320193a
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-05-21 12:07:47 +0530

    Detecting body parts
    It is able to detect my nose in real time.

--- pose.js
+++ pose.js
@@ -1,11 +1,27 @@
-let vidoe;
-
+let video;
+let poseNet;
+let results;
+let myPose;
 
 function setup() {
     createCanvas(1280,720);
-    vidoe = createCapture(VIDEO);
+    video = createCapture(VIDEO);
+    poseNet = ml5.poseNet(video, onReady);
+    poseNet.on('pose', (res)=>{
+        if(res.length>0){
+            results=res;
+            myPose = results[0].pose;
+        }
+    });
 
+}
+function onReady() {
+    console.log("OK")
 }
 function draw() {
     image(video,0,0);
+    fill(255,0,0);
+    if(myPose){
+        ellipse(myPose.nose.x, myPose.nose.y,50);
+    }
 }
